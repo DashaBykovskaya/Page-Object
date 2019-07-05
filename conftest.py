755526f672d67c2ae258140b89a2a3768fe77980
DashaBykovskaya <dbykovskaya.db@gmail.com>
2019-07-05 16:29:01 +0300

    modified conftest

--- conftest.py
+++ conftest.py
@@ -1,25 +1,24 @@
 import pytest
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en",
-                     help="Choose language ar de el es fr it nl pl ro ru sk uk en-gb")
-        
+    parser.addoption('--language', action='store', default="en", help="Choose language: ru, es, fr... Default: russian")
+
+
 @pytest.fixture(scope="function")
 def browser(request):
-    languages = "ar de el es fr it nl pl ro ru sk uk en-gb"
-    language = request.config.getoption("language")
-    if (language + " ") in languages:
-        print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option(
-            'prefs', {'intl.accept_languages': language})
-        browser = webdriver.Chrome(options=options)
-    else:
-        print("\nlanguage {} not supported :(\ntry: ar de el es fr it nl pl ro ru sk uk en-gb".format(language))
-        pytest.fail("Wrong Language")
+    lang = request.config.getoption("language")
+    print("\n--------------------------------------")
+    print(F"Check for language: {lang}")
+    
+    browser = webdriver.Chrome()
+	
+    link = f"http://selenium1py.pythonanywhere.com/{lang}/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(1)
+	
     yield browser
-    print("\nquit browser..")
     browser.quit()
+    print("\n--------------------------------------")
+    
